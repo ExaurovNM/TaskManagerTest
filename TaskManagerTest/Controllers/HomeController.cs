14dc4e78d9bbe57a641b57e7751e34b4f49e24ca
Nikita Exaurov <exaurov.nm@mail.ru>
2012-06-22 16:41:10 +0300

    TaskManagerSite
    Primitivs.

---
+++ TaskManagerTest/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaskManagerTest.Models;
+namespace TaskManagerTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            TasksDataManager tasksDataManager = new TasksDataManager();
+            ViewBag.Tasks = tasksDataManager.GetTasks();
+            return View(ViewBag.Tasks);
+        }
+        //edit
+        //----------------
+        //edit
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            TasksDataManager tasksDataManager = new TasksDataManager();
+
+            return View(tasksDataManager.GetById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Tasks task)
+        {
+            if(ModelState.IsValid)
+            {
+                TasksDataManager tasksDataManager = new TasksDataManager();
+                tasksDataManager.Edit(task);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        //edit-end
+
+        //add
+        //--------------
+        //add
+        [HttpGet]
+        public ActionResult Add()
+        {
+            TasksDataManager tasksDataManager = new TasksDataManager();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Tasks task)
+        {
+            if(ModelState.IsValid)
+            {
+                TasksDataManager tasksDataManager = new TasksDataManager();
+                tasksDataManager.Add(task);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        //add-end
+
+        //delete
+        //-------------
+        //delete
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            TasksDataManager tasksDataManager = new TasksDataManager();
+            return View(tasksDataManager.GetById(id));
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(Tasks task)
+        {
+                TasksDataManager tasksDataManager = new TasksDataManager();
+                tasksDataManager.Delete(task);
+
+                return RedirectToAction("Index");
+        }
+
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ TaskManagerTest/Models/TasksDataManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagerTest.Models
+{
+    public class TasksDataManager : IDisposable
+    {
+        private TasksDBEntities _db;
+
+        public TasksDataManager()
+        {
+            _db = new TasksDBEntities();
+        }
+
+        public IQueryable<Tasks> GetTasks()
+        {
+            return _db.Tasks;
+        }
+
+        public Tasks GetById(int id)
+        {
+            return _db.Tasks.SingleOrDefault(x => x.id == id);
+        }
+
+        public void Edit(Tasks task)
+        {
+            Tasks oldTask = GetById(task.id);
+            oldTask.Title = task.Title;
+            oldTask.Subject = task.Subject;
+            _db.SaveChanges();
+        }
+
+        public void Add (Tasks tasks)
+        {
+            _db.Tasks.AddObject(tasks);
+            _db.SaveChanges();
+        }
+
+        public void Delete(Tasks task)
+        {
+            Tasks oldTask = GetById(task.id);
+            _db.Tasks.DeleteObject(oldTask);
+            _db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
